e2cf227fd1295bf6d392d15c7bca746d1f27641d
surajvns1571999 <surajvns1571999@gmail.com>
2025-09-07 17:31:03 +0530

    add example14

---
+++ example14/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,23 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+
+public class Vehicle {
+    private String name = "Hundayi";
+
+    public Vehicle() {
+        System.out.println("Vehicle bean created by Spring");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
