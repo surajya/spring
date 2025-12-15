f8efb12a90753eec0b56570e2c291caa57d211c4
surajvns1571999 <surajvns1571999@gmail.com>
2025-12-16 05:28:07 +0530

    verify default behaviour of csrf protection of spring-security

--- example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecuringConfig.java
+++ example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecuringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,7 +16,8 @@ public class ProjectSecuringConfig {
     //permit all all requests
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requestMatcherRegistry) -> requestMatcherRegistry
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests((requestMatcherRegistry) -> requestMatcherRegistry
                 .requestMatchers("/","/home").permitAll()
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
